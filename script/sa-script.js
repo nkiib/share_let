bcfa1dfbba60924d10cc4425b902125cb805b703
NKIIB nishikiout <doskokimeil127.dosd@gmail.com>
2023-04-11 18:18:11 +0900

    first commit

---
+++ script/sa-script.js
@@ -0,0 +1,32 @@
+function getURL() {
+    return location.href;
+}
+  
+function getTitle() {
+    return document.title;
+}
+
+function copy2clipboard(form) {
+    if (!navigator.clipboard) {
+        alert("このブラウザは対応していません");
+        return;
+    }
+    navigator.clipboard.writeText(form).then(
+        () => {
+            alert("文章をコピーしました。");
+        },
+        () => {
+            alert("コピーに失敗しました。");
+        }
+    );
+  }
+  
+function Process() {
+    var url = getURL();
+    var title = getTitle();
+  
+    var share_form = title + "\n" + url;
+    document.getElementById("output").innerHTML = share_form;
+    
+    copy2clipboard(share_form);
+}
